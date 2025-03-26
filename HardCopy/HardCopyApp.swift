94b4de270744b2768516ad33d97bb9b894084d30
singhr7 <singhr7@mskcc.org>
2025-03-26 11:11:04 -0700

    Initial Commit

---
+++ HardCopy/HardCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HardCopyApp.swift
+//  HardCopy
+//
+//  Created by rsingh on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct HardCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HardCopyTests/HardCopyTests.swift
@@ -0,0 +1,17 @@
+//
+//  HardCopyTests.swift
+//  HardCopyTests
+//
+//  Created by rsingh on 3/26/25.
+//
+
+import Testing
+@testable import HardCopy
+
+struct HardCopyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
